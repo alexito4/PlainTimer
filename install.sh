ab507eff71d12e69da700a9e1509cd31fc8a84f2
Alejandro Martínez <alexito4@gmail.com>
2017-06-18 15:39:53 +0100

    publish OS version and modify script to build release

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo xcode-select --switch /Applications/Xcode-beta.app/
-swift build
+#swift build
+swift build -c release -Xswiftc -static-stdlib
 cp .build/debug/timer /usr/local/bin/timer
 #reset
 sudo xcode-select --switch /Applications/Xcode.app/
